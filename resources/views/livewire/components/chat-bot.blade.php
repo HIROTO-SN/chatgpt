621f440af5cba4baa59f62e8b40ca6219e9879dd
Hiroto N <hiroto4455656@gmail.com>
2025-07-02 05:46:43 +0900

    chat bot created

---
+++ resources/views/livewire/components/chat-bot.blade.php
@@ -0,0 +1,18 @@
+<div class="relative" x-data="{
+    text: '',
+    temporaryMessage: '',
+    sending: false
+}">
+    <div>
+        <ul class="mt-16 space-y-5 min-h-[65vh]">
+            <li class="flex justify-start">
+                <span class="flex-shrink-0 mr-2 inline-flex items-center justify-center">
+                    <img src="{{ auth()->user()->profile_photo_url }}" class="w-8 h-8 rounded-full"
+                        alt="{{ auth()->user()->name }}" />
+                </span>
+                <div class="bg-gray-300 text-black max-w-lg rounded-lg">
+
+                </div>
+            </li>
+    </div>
+</div>
