d1a925c365b1754d08ffc8079763108e443a17cb
Hiroto N <hiroto4455656@gmail.com>
2025-07-08 06:17:42 +0900

    shiki installed and get the title from DB

--- app/Livewire/Pages/Chat.php
+++ app/Livewire/Pages/Chat.php
@@ -8,8 +8,16 @@ use Livewire\Component;
 class Chat extends Component {
 
     public Conversation $conversation;
+    public string $title = 'New Chat';
+
+    public function mount() {
+        if($this->conversation->title) {
+            $this->title = $this->conversation->title;
+        }
+    }
 
     public function render() {
-        return view( 'livewire.pages.chat' );
+        return view( 'livewire.pages.chat' )
+            ->title($this->title);
     }
 }
